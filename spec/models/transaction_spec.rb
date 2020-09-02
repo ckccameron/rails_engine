291e7adfb706bb0e22001978e1f9b1226445c108
Cam C <ckc.cameron@gmail.com>
2020-09-02 17:04:05 -0600

    Write spec for Transaction model setup

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Transaction, type: :model do
+  it "exists with attributes" do
+    attrs = {
+      invoice_id: 2,
+      credit_card_number: "4580251236515201",
+      credit_card_expiration_date: ,
+      result: "success",
+      created_at: "2012-03-27 14:54:09 UTC",
+      updated_at: "2012-03-27 14:54:09 UTC"
+    }
+
+    transaction = Transaction.new(attrs)
+
+    expect(transaction).to be_a Transaction
+    expect(transaction.invoice_id).to eq(2)
+    expect(transaction.credit_card_number).to eq("4580251236515201")
+    expect(transaction.credit_card_expiration_date).to eq()
+    expect(transaction.result).to eq("success")
+    expect(transaction.created_at).to eq("2012-03-27 14:54:09 UTC")
+    expect(transaction.updated_at).to eq("2012-03-27 14:54:09 UTC")
+  end
+
+  # describe "relationships" do
+  #   it {should belong_to :invoice}
+  # end
+end
