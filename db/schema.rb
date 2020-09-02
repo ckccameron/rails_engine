30675733c180c16831b9f226b7c5b43411b4ca23
Cam C <ckc.cameron@gmail.com>
2020-09-02 15:43:40 -0600

    Realize I can just use timestamps for migrations so customers table is fixed now

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2020_09_02_212317) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
