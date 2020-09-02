e4b5014f996788c374b2891f5baa4e9081f937b2
Cam C <ckc.cameron@gmail.com>
2020-09-02 16:33:06 -0600

    Write spec for InvoiceItem model setup

---
+++ spec/models/invoice_item_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe InvoiceItem, type: :model do
+  it "exists with attributes" do
+    attrs = {
+      item_id: 963,
+      invoice_id: 33,
+      quantity: 2,
+      unit_price: 30323,
+      created_at: "2012-03-27 14:54:11 UTC",
+      updated_at: "2012-03-27 14:54:11 UTC"
+    }
+
+    invoice_item = InvoiceItem.new(attrs)
+
+    expect(invoice_item).to be_a InvoiceItem
+    expect(invoice_item.item_id).to eq(963)
+    expect(invoice_item.invoice_id).to eq(33)
+    expect(invoice_item.quantity).to eq(2)
+    expect(invoice_item.unit_price).to eq(30323)
+    expect(invoice_item.created_at).to eq("2012-03-27 14:54:11 UTC")
+    expect(invoice_item.).to eq(updated_at: "2012-03-27 14:54:11 UTC")
+  end
+end
