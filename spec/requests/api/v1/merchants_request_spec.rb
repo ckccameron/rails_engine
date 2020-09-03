10b387644c39865a2d2e166e4a261e781bcc0d25
Cam C <ckc.cameron@gmail.com>
2020-09-02 20:04:18 -0600

    Write spec for merchants index request

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "sends index of merchants" do
+    create_list(:merchant, 3)
+
+    get "/api/v1/merchants"
+
+    expect(response).to be_successful
+
+    body = JSON.parse(response.body)
+    expect(body["data"].count).to eq(3)
+  end
+end
