ebdaae9f7afae3361a94398bbc81550e69d44d06
Cam C <ckc.cameron@gmail.com>
2020-09-03 07:00:01 -0600

    Add index route for merchants finder controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
+        get '/find_all', controller: 'finder', action: 'index'
         get '/find', controller: 'finder', action: 'show'
       end
 
