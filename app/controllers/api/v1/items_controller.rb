adb939734746899115c587072a64cb949b941873
Cam C <ckc.cameron@gmail.com>
2020-09-02 22:57:11 -0600

    Add show action to items api controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
   end
+
+  def show
+    id = params[:id]
+    render json: ItemSerializer.new(Item.find(id))
+  end
 end
