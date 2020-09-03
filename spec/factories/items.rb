5e1084c704c80172b4f06b1190ff277c44fff5e4
Cam C <ckc.cameron@gmail.com>
2020-09-03 11:19:44 -0600

    Change item unit price in factory setup

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
    factory :item do
      name { Faker::Commerce.unique.product_name }
      description { Faker::Food.description }
-     unit_price { Faker::Number.number(digits: 5) }
+     unit_price { 15 }
    end
  end
