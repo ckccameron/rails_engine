0add4dacc288e4b7ecd300230fc847f258afdc64
Cam C <ckc.cameron@gmail.com>
2020-09-02 20:53:42 -0600

    Define private method for merchant params and add create action to merchants api controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -7,4 +7,14 @@ class Api::V1::MerchantsController < ApplicationController
     id = params[:id]
     render json: MerchantSerializer.new(Merchant.find(id))
   end
+
+  def create
+    render json: MerchantSerializer.new(Merchant.create(merchant_params))
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 end
