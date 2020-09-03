f038efee088bcffa61109dc74d7b7fadd7f41379
Cam C <ckc.cameron@gmail.com>
2020-09-03 06:39:05 -0600

    Add show route for merchants finder controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', controller: 'finder', action: 'show'
+      end
+
       resources :merchants do
         resources :items, only: [:index], controller: 'merchants/items'
       end
 
       resources :items do
-        get 'merchant', to: 'items/merchants#show', controller: 'items/merchants'
+        get '/merchant', to: 'items/merchants#show', controller: 'items/merchants'
       end
     end
   end
