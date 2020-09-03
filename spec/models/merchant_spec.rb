b76e97324e119f6a2aa2afb3ab0d8ed0849af30a
Cam C <ckc.cameron@gmail.com>
2020-09-03 11:20:23 -0600

    Add test for transactions relationship added in Merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -19,5 +19,6 @@ describe Merchant, type: :model do
   describe "relationships" do
     it {should have_many :items}
     it {should have_many :invoices}
+    it {should have_many(:transactions).through(:invoices)}
   end
 end
