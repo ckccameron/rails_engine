ca3be08cef2c30ce5d1ae141afc39ec2dbe7ae6b
Cam C <ckc.cameron@gmail.com>
2020-09-02 22:56:15 -0600

    Write spec for item show request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -17,4 +17,16 @@ describe "Items API" do
     expect(body[:data].count).to eq(3)
     expect(body[:data]).to_not be_empty
   end
+
+  it "sends item based on unique id" do
+    create :merchant
+    merchant = Merchant.last
+    id = create(:item, merchant: merchant).id
+
+    get "/api/v1/items/#{id}"
+
+    body = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(body[:data][:id]).to eq(id.to_s)
+  end
 end
