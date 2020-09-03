cac6ad3ef372d28cb02282b4c5a07bfefc0c02ad
Cam C <ckc.cameron@gmail.com>
2020-09-03 11:18:29 -0600

    Add relationships to models to help AR querying

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,19 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :transactions, through: :invoices
 
   def self.find_by_search_criteria(fragment)
     Merchant.where("name ILIKE ?", "%#{fragment}%")
   end
 
+  def self.most_revenue(param)
+    Merchant.select("merchants.*, sum(quantity * unit_price) as revenue")
+    .joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.successful)
+    .where("invoices.status = 'shipped'")
+    .group(:id)
+    .order("revenue desc")
+    .limit(param)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  has_one :customer, through: :invoice
+
+  scope :successful, -> { where(result: "success") }
 
 
 end
