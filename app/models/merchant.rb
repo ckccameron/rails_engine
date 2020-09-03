6575c2754f12770afbbd5eb64ae943f0f914490a
Cam C <ckc.cameron@gmail.com>
2020-09-03 06:40:14 -0600

    Add find by search criteria class method to Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,5 +2,11 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
 
+  def self.find_by_search_criteria(fragment)
+    Merchant.where("name ILIKE ?", "%#{fragment}%")
+  end
+
+  
+
 
 end
