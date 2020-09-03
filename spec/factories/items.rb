d4611b408af06f537ab899bf139ab6365b1ffb1a
Cam C <ckc.cameron@gmail.com>
2020-09-02 19:30:41 -0600

    Setup factories for items and merchants

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+   factory :item do
+     name { Faker::Commerce.unique.product_name }
+     description { Faker::Food.description }
+     unit_price {Faker::Number.number(digits: 5)}
+   end
+ end
