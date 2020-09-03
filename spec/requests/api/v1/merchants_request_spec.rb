ce9d1fdc77de64fe48159f3c66157cba4ec9bef2
Cam C <ckc.cameron@gmail.com>
2020-09-03 06:52:55 -0600

    Write spec for finding index of merchants that meet search query params

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -56,7 +56,7 @@ describe "Merchants API" do
   end
 
   describe "finders" do
-    it "can return a single merchant that matches set of criteria" do
+    it "can return a single merchant that matches search criteria" do
       create(:merchant, name: "In N Out")
       create(:merchant, name: "Chipotle")
       create(:merchant, name: "Sabor")
@@ -70,6 +70,27 @@ describe "Merchants API" do
 
       expect(search_body[:data]).to be_a(Hash)
       expect(name).to include("in")
+    end
+
+    it "can return index of merchants that match search criteria" do
+      create(:merchant, name: "King of Wakanda")
+      create(:merchant, name: "Burger King")
+      create(:merchant, name: "Kings & Queens")
+      create(:merchant, name: "Ace of Spades")
+
+      get "/api/v1/merchants/find?name=king"
+
+      expect(response).to be_successful
+
+      search_body = JSON.parse(response.body, symbolize_names: true)
+      names = search_body[:data].map do |merchant|
+        merchant[:attributes][:name].downcase
+      end
+
+      expect(search_body[:data]).to be_a(Hash)
+      expect(names).to include("king")
+      expect(names).to eq(["King of Wakanda", "Burger King", "Kings & Queens"])
+      expect(names).to_not eq(["Ace of Spades"])
     end
   end
 end
