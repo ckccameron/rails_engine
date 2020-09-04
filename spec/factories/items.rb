a5284baf50ba3d5947dbf1bf616c63aee81ebe05
Cam C <ckc.cameron@gmail.com>
2020-09-04 11:52:35 -0600

    Update unit price to a price float in items factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
    factory :item do
      name { Faker::Commerce.unique.product_name }
      description { Faker::Food.description }
-     unit_price { 15 }
+     unit_price { 15.00 }
    end
  end
